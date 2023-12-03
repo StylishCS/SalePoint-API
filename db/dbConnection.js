3ad1214c435f0656f386cb8624ab85014db65d99
StylishCS <joeshirf@gmail.com>
2023-12-03 19:28:52 +0200

    database connection issue fatal trace

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -20,6 +20,13 @@ function handleDisconnect() {
       throw err;
     }
   });
+  var del = connection._protocol._delegateError;
+  connection._protocol._delegateError = function (err, sequence) {
+    if (err.fatal) {
+      console.trace("fatal error: " + err.message);
+    }
+    return del.call(this, err, sequence);
+  };
 }
 
 handleDisconnect();
